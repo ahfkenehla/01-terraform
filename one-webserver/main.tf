751515f3e39dfd2f44c9541cf198f27ecccf2729
ahfkenehla <ahfkenehla@naver.com>
2022-12-23 16:05:25 +0900

    20221223

--- one-webserver/main.tf
+++ one-webserver/main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "example" {
 }
 
 resource "aws_security_group" "instance" {
-  name = "std01-terraform-example-instance"
+  name = var.security_group_name
 
   ingress {
     from_port   = var.server_port
--- one-webserver/variables.tf
+++ one-webserver/variables.tf
@@ -3,3 +3,8 @@ variable "server_port" {
   type        = number
   default     = 8080
 }
+
+variable "security_group_name" {
+  type    = string
+  default = "std01-terraform-example-instance"
+}
